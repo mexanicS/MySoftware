f5ea685d9c8e7fb77c7c512f66fc9f31683c4139
mexanicS <62372987+mexanicS@users.noreply.github.com>
2020-10-08 12:22:01 +0700

    Добавил переменные.

--- Мeasure/DataWindow1.xaml.cs
+++ Мeasure/DataWindow1.xaml.cs
@@ -43,6 +43,39 @@ namespace Мeasure
 
         private void Button_Click(object sender, RoutedEventArgs e)//нажатие на кнопку
         {
+            string a = NormalizedTemperature.Text;
+            int NT = Convert.ToInt32(a);
+
+            string b = DesignTemperature.Text;
+            int DT = Convert.ToInt32(b);
+
+            string c = ThermalConductivity1.Text;
+            int TC1 = Convert.ToInt32(c);
+
+            string d = ThermalConductivity2.Text;
+            int TC2 = Convert.ToInt32(d);
+
+            string f = LayerThickness1.Text;
+            int LT1 = Convert.ToInt32(f);
+
+            string g = LayerThickness2.Text;
+            int LT2 = Convert.ToInt32(g);
+
+            string h = TransmissionResistance.Text;
+            int TrR = Convert.ToInt32(h);
+
+            string j = HeatTransferCoefficientInside.Text;
+            int HTCI = Convert.ToInt32(j);
+
+            string k = HeatTransferCoefficientOutside.Text;
+            int HTCO = Convert.ToInt32(k);
+
+            string l = ThermalResistance.Text;
+            int ThR = Convert.ToInt32(l);
+
+            string m = ThermalResistanceClosedAirLayer.Text;
+            int TRCAL = Convert.ToInt32(m);
+
             this.Close();
 
             /*if (ButtonClicked != null)
